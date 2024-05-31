b9e3420525e05fb52c08dfa390b135db03852788
Armando Carrasco <210124@utxicotepec.edu.mx>
2024-05-31 00:01:44 -0600

    Commit de reestructuracion

---
+++ frontend/sistema-hospital/src/components/registerUser.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="bg-gray-100 flex h-screen items-center justify-center p-4">
+    <div class="w-full max-w-md">
+        <div class="bg-white shadow-md rounded-md p-8">
+
+            <img class="mx-auto h-12 w-auto" src="https://www.svgrepo.com/show/499664/user-happy.svg" alt="" />
+
+            <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                HOSPITAL PRIVILAGE CARE 
+                
+            </h2>
+            <P class="mt-3 text-center text-1xl font-bold tracking-tight text-gray-900"> REGISTRAR USUARIO</P>
+
+
+            <form class="space-y-6 mt-4" action="#" method="POST">
+                <div>
+                    <label for="password" class="block text-sm font-medium text-gray-700">Correo electrónico</label>
+                    <div class="mt-1">
+                        <input name="email" type="email-address" autocomplete="email-address" required
+                            class="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" />
+                    </div>
+                </div>
+
+                <div>
+                    <label for="password" class="block text-sm font-medium text-gray-700">Contraseña</label>
+                    <div class="mt-1">
+                        <input id="password" name="password" type="password" autocomplete="password" required
+                            class="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" />
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit"
+                        class="flex w-full justify-center rounded-md border border-transparent bg-sky-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2">ENTRAR
+                    </button>
+                    <P class="mt-3 text-center text-1xl font-bold tracking-tight text-gray-900"> O crear cuenta</P>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</template>
---
+++ frontend/sistema-hospital/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import RegisterUser from '@/components/registerUser.vue'
+import LoginView from '@/components/login.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterUser
+    }
+    
+  ]
+})
+
+export default router
