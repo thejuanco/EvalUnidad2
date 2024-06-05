f8dfa98d5be4462ba3856f73154d3ae751839682
Armando Carrasco <210124@utxicotepec.edu.mx>
2024-06-04 19:55:50 -0600

    Creacion de Vista Dashboard

--- frontend/sistema-hospital/src/router/index.js
+++ frontend/sistema-hospital/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import RegisterUser from '@/components/registerUser.vue'
 import LoginView from '@/components/login.vue'
+import DashboardView from '@/components/dashboard.vue'
 
 
 const router = createRouter({
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterUser
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView 
     }
     
   ]
