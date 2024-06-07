40beeeb7be1296b50e825d420966311f34a2def0
Mando CV <210124@utxicotepec.edu.mx>
2024-06-07 14:08:06 -0600

    actualizacion

--- frontend/sistema-hospital/src/components/dashboard.vue
+++ frontend/sistema-hospital/src/components/dashboard.vue
@@ -156,7 +156,7 @@ folder_managed
    
                      <li>
                         <RouterLink to="/estudio">
-                        <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Resultados de Estudios</a>
+                        <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Estudios</a>
                      </RouterLink>                      </li>
                      <li>
                         <RouterLink to="/resultadosEstudio">
